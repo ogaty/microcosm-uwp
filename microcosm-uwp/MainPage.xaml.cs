701a2edd7258b952b5a499a62c9da6d55ddd4087
sbook <admin@admin>
2017-10-14 11:38:33 +0900

    :tada:microcosm uwp version

---
+++ microcosm-uwp/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using microcosm.Calc;
+
+// 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
+
+namespace microcosm
+{
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        AstroCalc calc;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            calc = new AstroCalc(this);
+            calc.PositionCalc(9.0);
+        }
+    }
+}
