09f99ca3c34d3a21e8d9b8e809712079acf15b36
sbook <admin@admin>
2017-10-15 13:57:17 +0900

    :coffee:ephemeris file read

--- microcosm-uwp/Calc/AstroCalc.cs
+++ microcosm-uwp/Calc/AstroCalc.cs
@@ -1,23 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SwissEphNet;
 using System.IO;
 using System.Diagnostics;
-using System.Linq;
+using Windows.Storage;
+using Windows.Storage.Streams;
 
 namespace microcosm.Calc
 {
     public class AstroCalc
     {
         public SwissEph s;
+        public Stream swiss_stream;
 
         public AstroCalc(MainPage main)
         {
             s = new SwissEph();
         }
+
+        public static Stream GenerateStreamFromString(string s)
+        {
+            MemoryStream stream = new MemoryStream();
+            StreamWriter writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+
+        async Task<Stream> GetSwissFile(string fileName) {
+            StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/" + fileName));
+            IBuffer buffer = await FileIO.ReadBufferAsync(file);
+            byte[] buff2 = new byte[buffer.Length];
+            MemoryStream stream = new MemoryStream(buff2);
+
+            return stream;
+        }
+
 
         /// <summary>
         /// planet position calcurate.
@@ -25,12 +48,33 @@ namespace microcosm.Calc
         /// <param name="timezone">Timezone. JST=9.0</param>
         public void PositionCalc(double timezone)
         {
-            s.swe_set_ephe_path(Windows.Storage.ApplicationData.Current.LocalFolder.Path);
+            s.swe_set_ephe_path(Windows.Storage.ApplicationData.Current.LocalFolder.Path + "/ephe");
             s.OnLoadFile += (sender, e) => {
                 if (File.Exists(e.FileName))
                 {
                     e.File = new FileStream(e.FileName, FileMode.Open);
                 }
+                /*
+                string fileName = Path.GetFileName(e.FileName);
+
+                StorageFile file;
+                switch (fileName)
+                {
+                    case "seas_18.se1":
+                    case "sepl_18.se1":
+                    case "semo_18.se1":
+                        e.File = Task.Run(async () => await GetSwissFile(fileName)).Result;
+                        break;
+                    case "seleapsec.txt":
+                        Task.Run(async () => {
+                            file = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/" + fileName));
+                            e.File = GenerateStreamFromString(await FileIO.ReadTextAsync(file));
+                        });
+                        break;
+                    default:
+                        break;
+                }
+                */
             };
             int utc_year = 0;
             int utc_month = 0;
@@ -47,16 +91,17 @@ namespace microcosm.Calc
             s.swe_utc_to_jd(utc_year, utc_month, utc_day, utc_hour, utc_minute, utc_second, 1, dret, ref serr);
 
             int flag = SwissEph.SEFLG_SWIEPH | SwissEph.SEFLG_SPEED;
+//            flag = flag | SwissEph.SEFLG_HELCTR;
 
             // display設定で再計算させないようにあらかじめ計算しておく
 
             foreach (int planet_number in Common.CommonData.target_numbers)
             {
-
+                s.swe_calc_ut(dret[1], planet_number, flag, x, ref serr);
+                Debug.WriteLine(planet_number.ToString() + " " + x[0]);
+                Debug.WriteLine(serr);
             }
-            s.swe_calc_ut(dret[1], 0, flag, x, ref serr);
 
-            Debug.WriteLine(x[0]);
 
         }
     }
--- microcosm-uwp/MainPage.xaml.cs
+++ microcosm-uwp/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Diagnostics;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -12,8 +13,12 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.ApplicationModel;
 
 using microcosm.Calc;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using System.Threading.Tasks;
 
 // 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
 
@@ -30,8 +35,152 @@ namespace microcosm
         {
             this.InitializeComponent();
 
+            MainInit();
+        }
+
+        private async Task<bool> CreateSwiss()
+        {
+            var root = Windows.Storage.ApplicationData.Current.LocalFolder;
+            var ephe = await root.TryGetItemAsync("ephe");
+
+            if (ephe == null)
+            {
+                ephe = await root.CreateFolderAsync("ephe");
+            }
+
+            StorageFolder epheFolder = await root.GetFolderAsync("ephe");
+
+            if (await epheFolder.TryGetItemAsync("semo_18.se1") == null)
+            {
+                var semo = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/semo_18.se1")).AsTask().ConfigureAwait(false);
+                await semo.CopyAsync(epheFolder, "semo_18.se1", NameCollisionOption.FailIfExists);
+            }
+
+            if (await epheFolder.TryGetItemAsync("seas_18.se1") == null)
+            {
+                var seas = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/seas_18.se1")).AsTask().ConfigureAwait(false);
+                await seas.CopyAsync(epheFolder, "seas_18.se1", NameCollisionOption.FailIfExists);
+            }
+
+            if (await epheFolder.TryGetItemAsync("sepl_18.se1") == null)
+            {
+                var sepl = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/sepl_18.se1")).AsTask().ConfigureAwait(false);
+                await sepl.CopyAsync(epheFolder, "sepl_18.se1", NameCollisionOption.FailIfExists);
+            }
+
+            if (await epheFolder.TryGetItemAsync("se90377s.se1") == null)
+            {
+                var se90377s = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/se90377s.se1")).AsTask().ConfigureAwait(false);
+                await se90377s.CopyAsync(epheFolder, "se90377s.se1", NameCollisionOption.FailIfExists);
+            }
+
+            if (await epheFolder.TryGetItemAsync("s136108s.se1") == null)
+            {
+                var s136108s = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/s136108s.se1")).AsTask().ConfigureAwait(false);
+                await s136108s.CopyAsync(epheFolder, "s136108s.se1", NameCollisionOption.FailIfExists);
+            }
+
+            if (await epheFolder.TryGetItemAsync("s136199s.se1") == null)
+            {
+                var s136199s = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/s136199s.se1")).AsTask().ConfigureAwait(false);
+                await s136199s.CopyAsync(epheFolder, "s136199s.se1", NameCollisionOption.FailIfExists);
+            }
+
+            if (await epheFolder.TryGetItemAsync("s136472s.se1") == null)
+            {
+                var s136472s = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/s136472s.se1")).AsTask().ConfigureAwait(false);
+                await s136472s.CopyAsync(epheFolder, "s136472s.se1", NameCollisionOption.FailIfExists);
+            }
+
+            if (await epheFolder.TryGetItemAsync("seleapsec.txt") == null)
+            {
+                var seleapsec = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/seleapsec.txt")).AsTask().ConfigureAwait(false);
+                await seleapsec.CopyAsync(epheFolder, "seleapsec.txt", NameCollisionOption.FailIfExists);
+            }
+
             calc = new AstroCalc(this);
-            calc.PositionCalc(9.0);
+//            calc.PositionCalc(9.0);
+
+            return true;
+        }
+
+        private async Task<bool> CreateConfig()
+        {
+            var root = Windows.Storage.ApplicationData.Current.LocalFolder;
+            var system = await root.TryGetItemAsync("system");
+
+            if (system == null)
+            {
+                system = await root.CreateFolderAsync("system");
+            }
+
+            StorageFolder systemFolder = await root.GetFolderAsync("system");
+
+            if (await systemFolder.TryGetItemAsync("config.csm") == null)
+            {
+                var configFile = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/config.csm")).AsTask().ConfigureAwait(false);
+                await configFile.CopyAsync(systemFolder, "config.csm", NameCollisionOption.FailIfExists);
+            }
+
+            if (await systemFolder.TryGetItemAsync("setting0.csm") == null)
+            {
+                var setting0File = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/setting0.csm")).AsTask().ConfigureAwait(false);
+                await setting0File.CopyAsync(systemFolder, "setting0.csm", NameCollisionOption.FailIfExists);
+            }
+
+            if (await systemFolder.TryGetItemAsync("setting1.csm") == null)
+            {
+                var setting1File = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/setting1.csm")).AsTask().ConfigureAwait(false);
+                await setting1File.CopyAsync(systemFolder, "setting1.csm", NameCollisionOption.FailIfExists);
+            }
+
+            if (await systemFolder.TryGetItemAsync("setting2.csm") == null)
+            {
+                var setting2File = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/setting2.csm")).AsTask().ConfigureAwait(false);
+                await setting2File.CopyAsync(systemFolder, "setting2.csm", NameCollisionOption.FailIfExists);
+            }
+
+            if (await systemFolder.TryGetItemAsync("setting3.csm") == null)
+            {
+                var setting3File = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/setting3.csm")).AsTask().ConfigureAwait(false);
+                await setting3File.CopyAsync(systemFolder, "setting3.csm", NameCollisionOption.FailIfExists);
+            }
+
+            if (await systemFolder.TryGetItemAsync("setting4.csm") == null)
+            {
+                var setting4File = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/setting4.csm")).AsTask().ConfigureAwait(false);
+                await setting4File.CopyAsync(systemFolder, "setting4.csm", NameCollisionOption.FailIfExists);
+            }
+
+            if (await systemFolder.TryGetItemAsync("setting5.csm") == null)
+            {
+                var setting5File = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/setting5.csm")).AsTask().ConfigureAwait(false);
+                await setting5File.CopyAsync(systemFolder, "setting5.csm", NameCollisionOption.FailIfExists);
+            }
+
+            return true;
+        }
+
+        private async void MainInit()
+        {
+            List<Task<bool>> arrayTask = new List<Task<bool>>();
+            Task<bool> objTask = Task<bool>.Run(CreateSwiss);
+            arrayTask.Add(objTask);
+
+            await Task.WhenAll(arrayTask);
+        }
+
+        private async void FilePick()
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker();
+            picker.FileTypeFilter.Add(".txt");
+            Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
+            Debug.WriteLine(file.Path);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            FilePick();
         }
     }
 }
